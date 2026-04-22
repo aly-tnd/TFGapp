b4e9aa54fcf415606c3de5a1049a2b3f5491642c
Vidaltorres1 <vidaltorres.trabajo@gmail.com>
2026-04-23 00:11:34 +0200

    correcion eschema

--- backend/src/domain/apiGestion/infrastructure/db/user.schema.ts
+++ backend/src/domain/apiGestion/infrastructure/db/user.schema.ts
@@ -1,10 +1,9 @@
-import mongoose, { Schema } from 'mongoose'; // <--- Importación correcta
+import mongoose, { Schema } from 'mongoose';
 
 const UserSchema = new Schema({
-  _id: { type: String, required: true },
+  // ELIMINAMOS la línea del _id: { type: String, required: true }
   name: { type: String, required: true },
   email: { type: String, required: true },
-  labData: { type: Schema.Types.Mixed },
 });
 
 export const UserModel = mongoose.model("User", UserSchema);
